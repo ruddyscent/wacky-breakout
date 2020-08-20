b0f236db31688a859638cbb7969c8a4e48ae966f
Kyungwon Chun <ruddyscent@gmail.com>
2020-08-21 00:43:45 +0900

    Complete up to step 8 of week 1

---
+++ Assets/Scripts/Gameplay/Ball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody2D rb2D = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //float magnitude = ConfigurationUtils.BallImpulseForce;
+        //Vector2 direction = new Vector2(Mathf.Cos(20 * Mathf.PI / 180), Mathf.Sin(20 * Mathf.PI / 180));
+        rb2D = GetComponent<Rigidbody2D>();
+        //rb2D.velocity = magnitude * direction;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        float speed = rb2D.velocity.magnitude;
+        rb2D.velocity = speed * direction;
+    }
+}
---
+++ Assets/Scripts/Gameplay/Paddle.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    const float BounceAngleHalfRange = 60 * Mathf.PI / 180;
+
+    Rigidbody2D rb2D = null;
+    float halfColliderWidth = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        halfColliderWidth = GetComponent<BoxCollider2D>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float input = Input.GetAxis("Control");
+        Vector2 direction = new Vector2(input, 0);
+        float speed = ConfigurationUtils.PaddleMoveUnitsPerSecond;
+        Vector2 velocity = speed * direction;
+        Vector2 new_position = rb2D.position + velocity * Time.fixedDeltaTime;
+        new_position.x = CalculateClampedX(new_position.x);
+        rb2D.MovePosition(new_position);
+    }
+
+    float CalculateClampedX(float x)
+    {
+        if (x > ScreenUtils.ScreenRight - 2.2f * halfColliderWidth)
+        {
+            x = ScreenUtils.ScreenRight - 2.2f * halfColliderWidth;
+        }
+        else if (x < ScreenUtils.ScreenLeft + 2.2f * halfColliderWidth)
+        {
+            x = ScreenUtils.ScreenLeft + 2.2f * halfColliderWidth;
+        }
+
+        return x;
+    }
+
+    /// <summary>
+    /// Detects collision with a ball to aim the ball
+    /// </summary>
+    /// <param name="coll">collision info</param>
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.CompareTag("Ball"))
+        {
+            // calculate new ball direction
+            float ballOffsetFromPaddleCenter = transform.position.x -
+                coll.transform.position.x;
+            float normalizedBallOffset = ballOffsetFromPaddleCenter /
+                halfColliderWidth;
+            float angleOffset = normalizedBallOffset * BounceAngleHalfRange;
+            float angle = Mathf.PI / 2 + angleOffset;
+            Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+
+            // tell ball to set direction to new direction
+            Ball ballScript = coll.gameObject.GetComponent<Ball>();
+            ballScript.SetDirection(direction);
+        }
+    }
+}
