6ac2829bd593dde2b9de46041700296dcdaee901
Kyungwon Chun <ruddyscent@gmail.com>
2020-08-25 00:05:36 +0900

    Add block code and sprites

---
+++ Assets/Scripts/Gameplay/Block.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Block : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/BlockSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject spriteBlock0, spriteBlock1, spriteBlock2, spriteBlock3;
+    float width, height;
+    const float horizontalGap = 0.1f, verticalGap = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject tmpBlock = Instantiate(spriteBlock0);
+        width = tmpBlock.GetComponent<BoxCollider2D>().size.x;
+        height = tmpBlock.GetComponent<BoxCollider2D>().size.y;
+        Destroy(tmpBlock);
+
+        float x = ScreenUtils.ScreenLeft + horizontalGap + width / 2;
+        float y = 0;
+        while (x < ScreenUtils.ScreenRight - horizontalGap)
+        {
+            int selection = Random.Range(0, 4);
+            GameObject block;
+            switch (selection)
+            {
+                case 0:
+                    block = Instantiate(spriteBlock0, new Vector2(x, y), Quaternion.identity);
+                    break;
+                case 1:
+                    block = Instantiate(spriteBlock1, new Vector2(x, y), Quaternion.identity);
+                    break;
+                case 2:
+                    block = Instantiate(spriteBlock2, new Vector2(x, y), Quaternion.identity);
+                    break;
+                case 3:
+                    block = Instantiate(spriteBlock3, new Vector2(x, y), Quaternion.identity);
+                    break;
+            }
+            x += 2 * width + horizontalGap;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
