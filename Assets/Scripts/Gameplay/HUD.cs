12eeec404a461e797505f8a7502a3a3b286bd69e
Kyungwon Chun <ruddyscent@gmail.com>
2020-09-15 00:12:21 +0900

    Complete step 7 of increment 2

---
+++ Assets/Scripts/Gameplay/HUD.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField]
+    Text scoreText;
+    [SerializeField]
+    Text ballsLeftText;
+    int score;
+    int ballsLeft;
+    const string ScorePrefix = "Score: ";
+    const string BallsLeftPrefix = "Balls: ";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = ScorePrefix + score.ToString();
+        ballsLeftText.text = BallsLeftPrefix + ballsLeft.ToString();
+    }
+
+    public void AddPoints(int points)
+    {
+        score += points;
+        scoreText.text = ScorePrefix + score.ToString();
+    }
+
+    public void LooseBall()
+    {
+        ballsLeft -= 1;
+        ballsLeftText.text = BallsLeftPrefix + ballsLeft.ToString();
+    }
+}
