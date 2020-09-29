2be1d9dfd955fb090e421798d642947d082e2e44
Kyungwon Chun <ruddyscent@gmail.com>
2020-09-29 14:48:55 +0900

    Complete step 5 of increment 3
    Add a quit button to the main menu.

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void HandlePlayButtonOnClickEvent()
     {
-        
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void HandleQuitButtonOnClickEvent()
+    {
+        Application.Quit();
     }
 
     // Update is called once per frame
